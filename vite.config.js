d4bc43919271258519198ab4ce79916c2032d963
Melissa Balsante <mibsbalsante@gmail.com>
2023-02-22 16:24:02 -0300

    feat(styling): add container mixin and size related css vars

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: "",
+        additionalData: "@import '@ast/mixins.scss';",
       },
     },
   },
