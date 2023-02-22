a9acfa91abe1b17efa70bbab3245466ab4db9c86
Melissa Balsante <mibsbalsante@gmail.com>
2023-02-22 16:25:06 -0300

    feat(layout): add header styles and make it fixed at the top

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { useRouter } from "vue-router"
+import { useRouter, RouterLink } from "vue-router"
 
 import { useAuthStore, useAuthActions } from "@/stores/auth"
 
@@ -14,14 +14,87 @@ const handleLogout = () => {
 </script>
 
 <template>
-  <header>
-    <template v-if="username">
-      <p>
-        <span>{{ username }}</span>
-      </p>
-      <button @click="handleLogout">Logout</button>
-    </template>
+  <header class="header">
+    <nav class="header__container">
+      <RouterLink to="/" class="header__link-home">
+        <h1>Stock Market Stats</h1>
+      </RouterLink>
+
+      <template v-if="username">
+        <p class="header__username">
+          <span>{{ username }}</span>
+        </p>
+        <button class="header__logout" @click="handleLogout">Logout</button>
+      </template>
+    </nav>
   </header>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.header {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  background-color: var(--color-primary);
+  color: var(--color-white);
+  box-shadow: var(--shadow);
+
+  &__container {
+    @include container;
+
+    min-height: var(--size-header);
+    justify-content: space-between;
+    flex-wrap: wrap;
+    align-items: center;
+    gap: 4px 16px;
+
+    @media (max-width: 480px) {
+      padding-top: 8px;
+      padding-bottom: 8px;
+    }
+  }
+
+  &__link-home {
+    margin-right: auto;
+    text-decoration: none;
+    color: var(--color-white);
+
+    h1 {
+      margin: 0;
+      font-size: 1.5rem;
+    }
+
+    @media (max-width: 480px) {
+      width: 100%;
+      text-align: center;
+
+      h1 {
+        font-size: 1.2rem;
+      }
+    }
+  }
+
+  &__username {
+    margin: 0;
+    font-weight: 600;
+  }
+
+  &__logout {
+    background-color: var(--color-text);
+    color: var(--color-background);
+    border: 0 none;
+    border-radius: 100px;
+    padding: var(--spacing-form-elements);
+    font-weight: 600;
+    min-width: 120px;
+  }
+
+  &__username,
+  &__logout {
+    @media (max-width: 480px) {
+      font-size: var(--font-small);
+    }
+  }
+}
+</style>
