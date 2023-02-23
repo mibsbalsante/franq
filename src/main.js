c9378ced480c20aa8433542596d3135b0c19d007
Melissa Balsante <mibsbalsante@gmail.com>
2023-02-23 00:52:12 -0300

    chore(pwa): add a manifest file and different size icons for dowloading the app

--- src/main.js
+++ src/main.js
@@ -12,3 +12,17 @@ app.use(createPinia())
 app.use(router)
 
 app.mount("#app")
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(() => console.log("[Restaurant - SW] registered"))
+      .catch(err =>
+        console.log(
+          "[Restaurant - SW] not registered, see log for details",
+          err
+        )
+      )
+  })
+}
