93692adce6da43c27cc5bc4e2bc4db348fecf7fc
Melissa Balsante <mibsbalsante@gmail.com>
2023-02-22 23:36:59 -0300

    feat(layout): add login page styles

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -47,4 +47,53 @@ const handleSubmit = () => {
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.login {
+  @include container;
+
+  margin: auto;
+  max-width: 600px;
+
+  &__form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    background-color: var(--color-background-2);
+    padding: 32px 24px;
+    box-shadow: var(--shadow);
+  }
+
+  &__label {
+    font-weight: 600;
+    font-size: var(--font-small);
+    margin-bottom: -16px;
+  }
+
+  &__input,
+  &__submit {
+    padding: var(--spacing-form-elements);
+  }
+
+  &__input {
+    border: 0 none;
+    border-bottom: 1px solid var(--color-text);
+  }
+
+  &__submit {
+    margin: 16px auto 0;
+    background-color: var(--color-primary);
+    color: var(--color-background);
+    border: 0 none;
+    font-weight: 600;
+    border-radius: var(--border-rounded);
+    min-width: 120px;
+    opacity: 1;
+    transition: opacity 0.3s ease-in-out;
+
+    &[disabled] {
+      opacity: 0.6;
+    }
+  }
+}
+</style>
